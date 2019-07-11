9a0cce8e2c34ffe5569e8ddc263a66c91b6be889
SamsonLin <153011490@qq.com>
2019-07-11 09:55:02 +0800

    finish section-2 practice-2 and do it test

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  let map = collection.reduce((m, x) => m.set(x, (m.get(x) || 0) + 1), new Map());
+  let map = collection.reduce((m, x) => {
+    if(x.includes('-')){
+      return m.set(x, (m.get(x) || 0) + parseInt(x.substr(2)));
+    }else{
+      return m.set(x, (m.get(x) || 0) + 1);
+    }
+  }, new Map());
   let result=[];
   let newCollection=Array.from(map.keys());
   newCollection.forEach((item)=>{
-    result.push({'key':item,'count':map.get(item)})
+    result.push({'key':item.substr(0,1),'count':map.get(item)})
   })
   return result;
 }
