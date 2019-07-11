cea5c355b5c21e7dd8f9c6752aa60568ca38d68e
SamsonLin <153011490@qq.com>
2019-07-11 12:59:49 +0800

    finish section-2 practice-3 and do it test

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let map = collection.reduce((m, x) => {
+    if(x.includes('-')||x.includes(':')){
+      return m.set(x.slice(0,1), (m.get(x.slice(0,1)) || 0) + parseInt(x.substr(2)));
+    }else if(x.includes('[')){
+      return m.set(x.slice(0,1), (m.get(x.slice(0,1)) || 0) + parseInt(x.slice(2,x.length)));
+    }else{
+      return m.set(x.slice(0,1), (m.get(x) || 0) + 1);
+    }
+  }, new Map());
+  let result=[];
+  let newCollection=Array.from(map.keys());
+  newCollection.forEach((item)=>{
+    result.push({name:item,summary:map.get(item)})
+  })
+  return result;
 }
